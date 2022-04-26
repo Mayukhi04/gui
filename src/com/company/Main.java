d6836c002a2f002f717a83c279216c71701fc2cd
Mayukhi04 <pandamayukhi@gmail.com>
2022-04-26 10:03:11 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// demo gui
+	System.out.println("SEQUENCE: Program started");
+	JFrame frame = new JFrame("Demo Frame");
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setVisible(true);
+    }
+}
