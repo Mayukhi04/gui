f0febc55e447c2314677d4fd134f516b01e40174
Mayukhi04 <92730901+Mayukhi04@users.noreply.github.com>
2022-05-05 10:14:41 +0100

    Update GUI.java

--- src/com/company/GUI.java
+++ src/com/company/GUI.java
@@ -6,7 +6,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class GUI extends JPanel implements ActionListener, DocumentListener {
+public class GUI extends JPanel implements ActionListener, DocumentListener{
     // canvas for other GUI widgets
     JButton button1;
     JButton button2;
@@ -15,18 +15,47 @@ public class GUI extends JPanel implements ActionListener, DocumentListener {
         System.out.println("SEQUENCE: GUI Constructor");
         this.setPreferredSize(new Dimension(width, height));
         setLayout(null);
+
+        JLabel heading = new JLabel("Demo GUI");
+        heading.setBounds(0,0, 200, 40);
+        add(heading);
+
+        JScrollBar scrollBar = new JScrollBar();
+        scrollBar.setBounds(290,0, 10, 150);
+        add(scrollBar);
+
         button1 = new JButton("B1");
-        button1.setBounds(0,0, 100, 40);
+        button1.setBounds(0,40, 100, 40);
         button2 = new JButton("B2");
-        button2.setBounds(120,0, 100, 40);
-        JTextField username = new JTextField();
-        username.setBounds(0,60, 200, 40);
+        button2.setBounds(120,40, 100, 40);
         button1.addActionListener(this);
         button2.addActionListener(this);
-        username.getDocument().addDocumentListener(this);
         add(button1);
         add(button2);
+
+        JMenu menu, subMenu;
+        JMenuBar menuBar = new JMenuBar();
+        menu=new JMenu("Menu");
+        subMenu=new JMenu("Sub Menu");
+        JMenuItem i1, i2, i3, i4, i5;
+        i1 = new JMenuItem("Item 1");
+        i2 = new JMenuItem("Item 2");
+        i3 = new JMenuItem("Item 3");
+        i4 = new JMenuItem("Item 4");
+        i5 = new JMenuItem("Item 5");
+        menu.setBounds(210,100, 50, 40);
+        menu.add(i1); menu.add(i2); menu.add(i3);
+        subMenu.add(i4); subMenu.add(i5);
+        menu.add(subMenu);
+        menuBar.add(menu);
+        add(menuBar);
+        add(menu);
+
+        JTextField username = new JTextField();
+        username.setBounds(0,100, 200, 40);
+        username.getDocument().addDocumentListener(this);
         add(username);
+
     }
 
     @Override
@@ -52,4 +81,5 @@ public class GUI extends JPanel implements ActionListener, DocumentListener {
     public void changedUpdate(DocumentEvent e) {
         System.out.println("changed");
     }
+
 }
