246218c5d766fcd80350918096fbbd660860f73d
Mayukhi04 <pandamayukhi@gmail.com>
2022-05-17 10:11:05 +0100

    Commit

--- src/com/company/Basic.java
+++ src/com/company/Basic.java
@@ -1,22 +1,46 @@
 package com.company;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Basic extends JPanel {
+import static java.awt.Color.green;
+
+public class Basic extends JPanel implements ActionListener {
 
     private JFrame frame;
+    private JButton makeGreen;
+    private String colour = "red";
 
     public Basic(int width, int height) {
         // initialise the window
         frame = new JFrame("Demo");
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        frame.setSize(300, 300);
+        frame.setSize(width, height);
 
         // create the canvas that will hold the actual graphics
         frame.getContentPane().add(this);
 
+        makeGreen = new JButton("make green");
+        makeGreen.setBounds(0,40, 100, 40);
+        makeGreen.addActionListener(this);
+        add(makeGreen);
+
+        this.repaint();
+
         // display the frame AFTER adding the panel to prevent drawing glitches
         frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand() == "make green") {
+//            Graphics g = null;
+//            this.makeGreen(Graphics g);
+            System.out.println("click");
+            colour = "green";
+        }
+
     }
 
     @Override
@@ -25,7 +49,21 @@ public class Basic extends JPanel {
         int top = 80;
         int width = 40;
         int height = 150;
-        g.setColor(Color.red);
+        if (colour.equals("Green")) {
+            g.setColor(Color.green);
+        } else {
+            g.setColor(Color.red);
+        }
+
+        g.fillRect(left, top, width, height);
+    }
+
+    public void makeGreen(Graphics g) {
+        int left = 50;    // hard-coded just for testing
+        int top = 80;
+        int width = 40;
+        int height = 150;
+        g.setColor(green);
         g.fillRect(left, top, width, height);
     }
 
